70ac9bc971a7a201445719db898bbd05fc6a3d54
anonymouslichen <90940282+anonymouslichen@users.noreply.github.com>
2024-10-29 14:29:11 -0500

    Update run_Braker.sh

--- annotation_Braker/run_Braker.sh
+++ annotation_Braker/run_Braker.sh
@@ -8,15 +8,15 @@
 #SBATCH --time=48:00:00
 #SBATCH --array=1-34                  #Adjust to 0-#of genomes (I ran a test on index 0)
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=meye2099@umn.edu
+#SBATCH --mail-user=                  #Add your email if you wish to receive updates
 
-# Change to the desired working directory
-cd /home/stan0477/meye2099
+# Move to working directory (called upon later to mount to your Singularity container)
+cd /your/working/directory
 
 # Directory containing all the genome files in FASTA format
-GENOMES_DIR=Algae_Evolution/Trebouxiophyceae/SoftMasked
-OUTPUT_DIR=Algae_Evolution/Trebouxiophyceae/Braker
-export AUGUSTUS_CONFIG_PATH=/home/stan0477/meye2099/bin/Augustus/config
+GENOMES_DIR=/your/genome/directory
+OUTPUT_DIR=your/output/directory
+export AUGUSTUS_CONFIG_PATH=/your/writable/augustus/config/directory          #You will need to copy the config folder from within the singularity container to someplace writeable
 
 # Path to BRAKER Singularity image
 BRAKER_SIF=bin/braker3.sif
@@ -39,7 +39,7 @@ singularity exec --bind ${PWD}:${PWD} $BRAKER_SIF braker.pl --genome=$genome \
           --workingdir=$OUTPUT_DIR/$genome_name \
           --softmasking \
           --gff3 \
-          --prot_seq=Algae_Evolution/Viridiplantae.fa \
+          --prot_seq=Viridiplantae.fa \          #Add your desired protein file: https://bioinf.uni-greifswald.de/bioinf/partitioned_odb11/
           --useexisting \
           --threads=8 \
           --AUGUSTUS_CONFIG_PATH=$AUGUSTUS_CONFIG_PATH
