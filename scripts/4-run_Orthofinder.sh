80b2ab6fbdbf962a8a798dd08d492583901592cc
anonymouslichen <90940282+anonymouslichen@users.noreply.github.com>
2025-10-08 12:00:10 -0500

    Create 4-run_Orthofinder.sh

---
+++ scripts/4-run_Orthofinder.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=orthofinder
+#SBATCH --mem=32G
+#SBATCH --time=4:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --output=orthofinder_%A.out
+#SBATCH --error=orthofinder_%A.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=youremail
+
+# Load OrthoFinder module 
+module load orthofinder
+
+# Define working directory 
+WORKDIR=/home/stan0477/meye2099/Algae_Evolution/Trebouxiophyceae
+ORTHO_INPUT=${WORKDIR}/Orthofinder
+
+# Create a results directory if it doesn’t exist
+mkdir -p ${ORTHO_INPUT}
+
+# Move into working directory
+cd ${WORKDIR}
+
+# Run OrthoFinder
+orthofinder -f ${ORTHO_INPUT} -t ${SLURM_CPUS_PER_TASK}
